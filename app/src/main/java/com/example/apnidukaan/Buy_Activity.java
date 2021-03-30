faf64f04d22c38b2c41b63a5d5e72bb2c6ebe580
Sourabh kourav <55892270+sourabh065@users.noreply.github.com>
2021-03-30 20:19:09 +0530

    All other activities created

---
+++ app/src/main/java/com/example/apnidukaan/Buy_Activity.java
@@ -0,0 +1,23 @@
+package com.example.apnidukaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class Buy_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+
+    }
+}
--- app/src/main/java/com/example/apnidukaan/MainActivity.java
+++ app/src/main/java/com/example/apnidukaan/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.apnidukaan;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button buyButton, sellButton, viewDataButton, exportDataButton, transactionButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buyButton = findViewById(R.id.buy_button);
+        sellButton = findViewById(R.id.sell_button);
+        viewDataButton = findViewById(R.id.viewdata_button);
+        exportDataButton = findViewById(R.id.exportdata_button);
+        transactionButton = findViewById(R.id.transaction_button);
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button Pressed =>> ","buy button");
+                Intent intent = new Intent(getApplicationContext(), Buy_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button Pressed =>> ","sell button");
+                Intent intent = new Intent(getApplicationContext(), Sell_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button Pressed =>> ","view data button");
+                Intent intent = new Intent(getApplicationContext(), ViewData_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        exportDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button Pressed =>> ","export data button");
+                Intent intent = new Intent(getApplicationContext(), ExportData_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        transactionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Button Pressed =>> ","transaction button");
+                Intent intent = new Intent(getApplicationContext(), Transaction_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
