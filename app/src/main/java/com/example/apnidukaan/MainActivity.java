078fa4f1af1c2f216a1ef12e7b7346a4ead322ba
Sourabh kourav <55892270+sourabh065@users.noreply.github.com>
2021-05-23 13:06:36 +0530

    soe more ui elements added

--- app/src/main/java/com/example/apnidukaan/MainActivity.java
+++ app/src/main/java/com/example/apnidukaan/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button buyButton, sellButton, viewDataButton, exportDataButton, transactionButton ;
+    Button buyButton, sellButton, viewDataButton, transactionButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         buyButton = findViewById(R.id.buy_button);
         sellButton = findViewById(R.id.sell_button);
         viewDataButton = findViewById(R.id.viewdata_button);
-        exportDataButton = findViewById(R.id.exportdata_button);
         transactionButton = findViewById(R.id.transaction_button);
 
         buyButton.setOnClickListener(new View.OnClickListener() {
@@ -45,15 +44,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.i("Button Pressed =>> ","view data button");
                 Intent intent = new Intent(getApplicationContext(), ViewData_Activity.class);
-                startActivity(intent);
-            }
-        });
-
-        exportDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("Button Pressed =>> ","export data button");
-                Intent intent = new Intent(getApplicationContext(), ExportData_Activity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/apnidukaan/ViewData_Activity.java
+++ app/src/main/java/com/example/apnidukaan/ViewData_Activity.java
@@ -2,13 +2,66 @@ package com.example.apnidukaan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ViewData_Activity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ViewData_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    ListView list ;
+    String[] data = {"apple","guava","mango","pineapple","orange","banana","grapes","apple","guava",
+            "mango","pineapple","orange","banana","grapes","apple","guava","mango","pineapple","orange","banana","grapes"};
+
+    String[] sort = {"a to z","z to a","Quantity less first","Quantity high first"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewdata);
+
+        list = findViewById(R.id.listView);
+        //list adapter here
+        showData();
+
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,sort);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+    }
+
+    public void showData(){
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, data);
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),sort[position] , Toast.LENGTH_LONG).show();
+        switch (position){
+            case 0:
+                Arrays.sort(data);
+                showData();
+                break;
+            case 1:Arrays.sort(data, Collections.reverseOrder());
+                showData();
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
